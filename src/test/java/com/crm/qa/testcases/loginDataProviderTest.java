052b16ad3cb18efd4d4faafa74a75a198545f794
Tony Lu <xudong.lu@yahoo.com>
2019-03-26 23:03:11 +0800

    a dot change

--- src/test/java/com/crm/qa/testcases/loginDataProviderTest.java
+++ src/test/java/com/crm/qa/testcases/loginDataProviderTest.java
@@ -27,9 +27,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class loginDataProviderTest {
 	
 	
+}
 	
-	
-	public static void main(String[] args) throws IOException {	
+	//public static void main(String[] args) throws IOException {	
 		//System.setProperty("webdriver.chrome.driver", "F:\\toolDownloaded\\chromedriver.exe");
 	
 //	String projectPath = System.getProperty("user.dir");
@@ -38,41 +38,41 @@ public class loginDataProviderTest {
 
 	//System.out.println(data[0][1]);
 			
-		File xlsFile = new File("F:\\Java\\CRMTest\\src\\main\\java\\com\\crm\\qa\\testdata\\testData.xlsx");
-		 
-		// 工作表
-		Workbook workbook = WorkbookFactory.create(xlsFile);
- 
-		// 表个数。
-		int numberOfSheets = workbook.getNumberOfSheets();
- 
-		// 遍历表。
-		for (int i = 0; i < numberOfSheets; i++) {
-			Sheet sheet = workbook.getSheetAt(i);
- 
-			// 行数。
-			int rowNumbers = sheet.getLastRowNum() + 1;
- 
-			// Excel第一行。
-			Row temp = sheet.getRow(0);
-			if (temp == null) {
-				continue;
-			}
- 
-			int cells = temp.getPhysicalNumberOfCells();
- 
-			// 读数据。
-			for (int row = 0; row < rowNumbers; row++) {
-				Row r = sheet.getRow(row);
-				for (int col = 0; col < cells; col++) {
-					System.out.print(r.getCell(col).toString()+" ");
-				}
- 
-				// 换行。
-				System.out.println();
-			}
-		}
-	}
+//		File xlsFile = new File("F:\\Java\\CRMTest\\src\\main\\java\\com\\crm\\qa\\testdata\\testData.xlsx");
+//		 
+//		// 工作表
+//		Workbook workbook = WorkbookFactory.create(xlsFile);
+// 
+//		// 表个数。
+//		int numberOfSheets = workbook.getNumberOfSheets();
+// 
+//		// 遍历表。
+//		for (int i = 0; i < numberOfSheets; i++) {
+//			Sheet sheet = workbook.getSheetAt(i);
+// 
+//			// 行数。
+//			int rowNumbers = sheet.getLastRowNum() + 1;
+// 
+//			// Excel第一行。
+//			Row temp = sheet.getRow(0);
+//			if (temp == null) {
+//				continue;
+//			}
+// 
+//			int cells = temp.getPhysicalNumberOfCells();
+// 
+//			// 读数据。
+//			for (int row = 0; row < rowNumbers; row++) {
+//				Row r = sheet.getRow(row);
+//				for (int col = 0; col < cells; col++) {
+//					System.out.print(r.getCell(col).toString()+" ");
+//				}
+// 
+//				// 换行。
+//				System.out.println();
+//			}
+//		}
+//	}
 
 
 			
@@ -117,4 +117,4 @@ public class loginDataProviderTest {
 //	}
 	
 		
-}
+//}
